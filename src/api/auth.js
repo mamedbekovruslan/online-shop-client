ee813dc24b048e2f5795e2a40d315513d6c24238
mamedbekovRA <mamedbekovruslan@yandex.ru>
2025-03-09 12:27:40 +0300

    поменять baseUrl

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://89.111.170.174:3000/api",
 });
 
 API.interceptors.request.use((req) => {
